f69be90da2f493901d8235584e49ae8f8ffd2f6b
Badal Kumar Behera <surajbadal69@gmail.com>
2023-11-22 17:17:31 +0530

    edit controller file and add restcontroller

--- src/main/java/com/example/demo/web/UserRegistrationController.java
+++ src/main/java/com/example/demo/web/UserRegistrationController.java
@@ -1,42 +1,29 @@
 package com.example.demo.web;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.service.UserService;
 import com.example.demo.web.dto.UserRegistraionDto;
 
-@Controller
-@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+@CrossOrigin(origins = "http://localhost:3000") // Update with your React app's URL
 @RequestMapping("api/v1/user")
 public class UserRegistrationController {
-	
-	private UserService userService;
 
-	public UserRegistrationController(UserService userService) {
-		super();
-		this.userService = userService;
-	}
-	
-	@ModelAttribute("user")
-	public UserRegistraionDto userRegistraionDto() {
-		return new UserRegistraionDto();
-	}
-	
-	@GetMapping
-	public String showRegistrationForm() {
-		return "registration";
-	}
-	
-	@PostMapping
-	public String registerUserAccount(@ModelAttribute("user") UserRegistraionDto registraionDto) {
-		userService.save(registraionDto);
-		
-		return "/redirect:/register";
-	}
+    private final UserService userService;
 
+    public UserRegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Void> registerUserAccount(@RequestBody UserRegistraionDto registraionDto) {
+        userService.save(registraionDto);
+        return ResponseEntity.ok().build();
+    }
 }
